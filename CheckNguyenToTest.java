691116ebcfd029168eaa970303b7a0d1566238b3
tgiang04 <killkazaki2k4@gmail.com>
2024-04-16 15:16:18 +0700

    Ca kiem thu bao phu tat ca cac lenh

---
+++ CheckNguyenToTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CheckNguyenToTest {
+
+    @Test
+    public void testIsPrimeWithPrimeNumber() {
+        assertTrue(CheckNguyenTo.isPrime(2));
+        assertTrue(CheckNguyenTo.isPrime(3));
+        assertTrue(CheckNguyenTo.isPrime(5));
+        assertTrue(CheckNguyenTo.isPrime(7));
+        assertTrue(CheckNguyenTo.isPrime(11));
+        assertTrue(CheckNguyenTo.isPrime(13));
+    }
+
+    @Test
+    public void testIsPrimeWithNonPrimeNumber() {
+        assertFalse(CheckNguyenTo.isPrime(1));
+        assertFalse(CheckNguyenTo.isPrime(4));
+        assertFalse(CheckNguyenTo.isPrime(6));
+        assertFalse(CheckNguyenTo.isPrime(8));
+        assertFalse(CheckNguyenTo.isPrime(9));
+        assertFalse(CheckNguyenTo.isPrime(10));
+    }
+
+    @Test
+    public void testIsPrimeWithZero() {
+        assertFalse(CheckNguyenTo.isPrime(0));
+    }
+
+    @Test
+    public void testIsPrimeWithNegativeNumber() {
+        assertFalse(CheckNguyenTo.isPrime(-1));
+        assertFalse(CheckNguyenTo.isPrime(-5));
+        assertFalse(CheckNguyenTo.isPrime(-10));
+    }
+}
